d690366531da16cc0083ec5a08f6efe944708cd2
IDomino-1 <18895557307@163.com>
2020-11-11 19:46:35 +0800

    新增注册组件   路由方法

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
-import {Button,Carousel,CarouselItem} from 'element-ui'
+import {Button,Carousel,CarouselItem, 
+    Form,FormItem,Input,Radio,RadioGroup,Notification} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Carousel)
 Vue.use(CarouselItem)
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+
+Vue.prototype.$notify = Notification; //提示弹窗组件
