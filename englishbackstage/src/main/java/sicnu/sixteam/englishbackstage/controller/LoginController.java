4f6871cdd3363639d56fab88bf1cd5057d62c4d7
Sisimai-Rinka <chuanxuemenglihua@gmail.com>
2020-11-15 20:45:35 +0800

    Revert "Revert "登录借口编写完成""
    This reverts commit 1cdb3523028f39d4a66ccb5fd8a84263830023fe.

--- englishbackstage/src/main/java/sicnu/sixteam/englishbackstage/controller/LoginController.java
+++ englishbackstage/src/main/java/sicnu/sixteam/englishbackstage/controller/LoginController.java
@@ -3,32 +3,36 @@ package sicnu.sixteam.englishbackstage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sicnu.sixteam.englishbackstage.model.User;
-import sicnu.sixteam.englishbackstage.service.LoginService;
+import sicnu.sixteam.englishbackstage.service.UserService;
 
 import javax.annotation.Resource;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-//用户登录接口s
 @Controller
 public class LoginController {
 
     @Resource
-    private LoginService loginService;
-    private User user;
+    private UserService userService;
+
     /**
      *
-     * @param loginid                    //用于登录的用户信息（邮箱，手机号）
-     * @param password                   //用户密码
+     * @param username
+     * @param emailnum
+     * @param password
      */
-    @RequestMapping(value = "/login",method = POST)
+    @RequestMapping("/RegisterByEmail")
     @ResponseBody
-    private int RegisterByEmail(String loginid,String password) {
-        user = loginService.checkuser(loginid,password);
-        if (user != null)
-            return 0;                                                //登录成功返回0
-        return -1;                                                   //登录失败返回-1
+    private void RegisterByEmail(String username,String emailnum,String password) {
+         userService.InsertNewUserByEmail(username,emailnum,password);
+    }
+
+    /**
+     *
+     * @param username
+     * @param phonenum
+     * @param password
+     */
+    @RequestMapping("/login")
+    @ResponseBody
+    private void RegisterByPhone(String username,String phonenum,String password) {
+         userService.InsertNewUserByPhone(username,phonenum,password);
     }
 }
-
--- englishbackstage/src/main/java/sicnu/sixteam/englishbackstage/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package sicnu.sixteam.englishbackstage.service;
-
-import sicnu.sixteam.englishbackstage.model.User;
-
-//用户登录接口
-public interface LoginService {
-    User checkuser(String phonenum, String password);
-}
-
