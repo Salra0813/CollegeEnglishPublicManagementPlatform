086d16610358eb7cdef8b35393be39526f60021d
Sisimai-Rinka <chuanxuemenglihua@gmail.com>
2020-11-15 20:44:32 +0800

    登录借口编写完成

--- englishbackstage/src/main/java/sicnu/sixteam/englishbackstage/service/Impl/UserServiceImpl.java
+++ englishbackstage/src/main/java/sicnu/sixteam/englishbackstage/service/Impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ package sicnu.sixteam.englishbackstage.service.Impl;
 
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import sicnu.sixteam.englishbackstage.mapper.UserMapper;
 import sicnu.sixteam.englishbackstage.model.User;
 import sicnu.sixteam.englishbackstage.service.UserService;
